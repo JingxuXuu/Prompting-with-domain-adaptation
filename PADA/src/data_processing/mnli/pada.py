89068b3445624cd23ede6e6eb5ddaf26f518bba3
Jingxu <59519977+JingxuXuu@users.noreply.github.com>
2022-09-30 19:05:03 -0700

    Add files via upload

---
+++ PADA/src/data_processing/mnli/pada.py
@@ -0,0 +1,127 @@
+from collections import defaultdict
+from numpy import percentile
+from random import random
+from pathlib import Path
+from typing import List, Union, Dict
+from torch.utils.data import Dataset, DataLoader
+from transformers import T5TokenizerFast, BatchEncoding
+from src.data_processing.rumor.base import RumorDataProcessor
+from src.data_processing.rumor.base_unlabelled import RumorunlabelledDataProcessor
+from src.data_processing.mnli.base_unlabelled import MNLIunlabelledDataProcessor
+from src.utils.constants import DATA_DIR, EXP_DIR
+import torch
+
+
+class MNLIPadaDataProcessor(MNLIunlabelledDataProcessor):
+
+    def __init__(self, src_domains: List[str], trg_domain: str, data_dir: Union[str, Path], experiment_dir: Union[str, Path]):
+        super().__init__(src_domains, trg_domain, data_dir)
+        self.experiment_dir = experiment_dir
+        for split in MNLIunlabelledDataProcessor.ALL_SPLITS:
+            self.data[split]["drfs"] = self._read_drfs(split)
+
+    def _read_drfs(self, mode: str = 'train'):
+        domains = self.src_domains if mode != 'test' else [self.trg_domain]
+        if mode == 'test' or mode == 'dev':
+            return ['' for _ in range(len(self.data[mode]["input_str"]))]
+        all_drfs = list()
+        for domain in domains:
+            prompts_path = Path(self.experiment_dir) / "mnli" / self.trg_domain / "prompt_annotations" / domain
+            with open(prompts_path / f"annotated_prompts_train.pt", "rb") as f:
+                drfs = torch.load(f)
+            all_drfs.extend(drfs)
+        return all_drfs
+
+
+
+
+class MNLIPadaDataset(Dataset):
+
+    DOMAIN_PROMPT = "domain"
+    MNLI_PROMPT = "mnli"
+    DRF_DELIMITER = ", "
+
+    def __init__(self, split: str, data_processor: MNLIPadaDataProcessor, tokenizer: T5TokenizerFast,
+                 max_seq_len: int = 512, max_drf_seq_len: int = 20, mixture_alpha: float = 0.2):
+        self.split = split
+        self.data_processor = data_processor
+        self.tokenizer = tokenizer
+        self.max_seq_len = max_seq_len
+        self.max_drf_seq_len = max_drf_seq_len
+        self.mixture_alpha = mixture_alpha
+        self.tokenized_data = self._init_tokenized_data(split, data_processor)
+
+    def __len__(self):
+        if self.split == "train":
+            return self.tokenized_data["num_examples"]
+        else:
+            return len(self.tokenized_data["example_id"])
+
+    def __getitem__(self, index):
+        if self.split == "train":
+            if random() <= self.mixture_alpha:
+                tokenized_data = self.tokenized_data["domain"]
+            else:
+                tokenized_data = self.tokenized_data["drf"]
+        else:
+            tokenized_data = self.tokenized_data
+        return {
+            k: v[index]
+            for k, v in tokenized_data.items()
+        }
+
+    def tokenize_input_str(self, input_str: List[List[str]]) -> BatchEncoding:
+        return self.tokenizer(input_str, is_split_into_words=False,
+                              padding='max_length', truncation=True,
+                              max_length=self.max_seq_len, return_tensors="pt", return_attention_mask=True)
+
+    def tokenize_output_str(self, output_str: List[List[str]]) -> BatchEncoding:
+        return self.tokenizer(output_str, is_split_into_words=False, padding='max_length', truncation=True,
+                              max_length=self.max_drf_seq_len, return_tensors="pt")["input_ids"]
+
+    @staticmethod
+    def _build_training_tokenized_data(data):
+        domain_data = defaultdict(list)
+        drf_data = defaultdict(list)
+        for i in range(len(data["example_id"])):
+            drfs_str = f"{data['domain_label'][i]} - " + f"{MNLIPadaDataset.DRF_DELIMITER.join(data['drfs'][i])}:"
+            prompt = f"{MNLIPadaDataset.DOMAIN_PROMPT}: "
+            domain_data["input_str"].append(prompt + "premise:" + data["input_str"][i][0] + " hypothesis: " + data["input_str"][i][1])
+            domain_data["output_str"].append(drfs_str)
+            domain_data["output_labels"].append(data["output_label"][i])
+            domain_data["prompt"].append(prompt)
+
+            prompt = f"{drfs_str}: "
+            drf_data["input_str"].append(prompt + "premise:" + data["input_str"][i][0] + " hypothesis: " + data["input_str"][i][1])
+            drf_data["output_str"].append("")
+            drf_data["output_labels"].append(data["output_label"][i])
+            drf_data["prompt"].append(prompt)
+        return {"domain": domain_data, "drf": drf_data}
+
+    def _init_tokenized_data(self, split, data_processor) -> Union[BatchEncoding, Dict[str, BatchEncoding]]:
+        data = data_processor.get_split_data(split)
+        if split == "train":
+            tokenized_data_dict = dict(num_examples=len(data["example_id"]))
+            for key, data_dict in self._build_training_tokenized_data(data).items():
+                tokenized_data = self.tokenize_input_str(data_dict["input_str"])
+                tokenized_data["prompt_output_ids"] = self.tokenize_output_str(data_dict["output_str"])
+                tokenized_data["output_label_id"] = data_dict["output_labels"]
+                tokenized_data["prompt"] = data_dict["prompt"]
+                tokenized_data["input_str"] = data["input_str"]
+                tokenized_data["domain_label"] = data["domain_label"]
+                tokenized_data["drfs"] = data["drfs"]
+                tokenized_data["example_id"] = data["example_id"]
+                tokenized_data_dict[key] = tokenized_data
+            return tokenized_data_dict
+        else:
+            tokenized_data = self.tokenize_input_str(data["input_str"])
+            tokenized_data["output_label_id"] = data["output_label"]
+            tokenized_data["input_str"] = data["input_str"]
+            tokenized_data["domain_label"] = data["domain_label"]
+            tokenized_data["drfs"] = data["drfs"]
+            tokenized_data["example_id"] = data["example_id"]
+        return tokenized_data
+
+
+
+
---
+++ PADA/src/data_processing/review/base_unlabelled.py
@@ -0,0 +1,142 @@
+from numpy import percentile
+from pathlib import Path
+from collections import defaultdict
+from typing import Dict, List, Union, Tuple
+from torch.utils.data import Dataset, DataLoader
+from transformers import T5TokenizerFast
+from PADA.src.utils.constants import DATA_DIR
+import pickle
+import json
+import xml.etree.ElementTree as ET
+
+
+class ReviewunlabelledDataProcessor:
+    ALL_SPLITS = ("train", "dev", "test")
+    WORD_DELIMITER = " "
+
+    def __init__(self, src_domains: List[str], trg_domain: str, data_dir: Union[str, Path]):
+        self.data_dir = data_dir
+        self.src_domains = src_domains
+        self.trg_domain = trg_domain
+        self.reduce_label_dict = {
+            1: 1,
+            0: 0
+        }
+        self.labels_dict = {
+            0: 0,
+            1: 1
+        }
+        self.data = self.load_data()
+
+    def read_data_from_file(self, mode: str = 'train') -> Dict[str, List[Union[str, List[str], Tuple[int]]]]:
+        domains = self.src_domains if mode != 'test' else [self.trg_domain]
+        data_dict = defaultdict(list)
+        if mode == 'dev':
+            data_dict = defaultdict(list)
+            for domain_idx, domain in enumerate(domains):
+                data_path = Path(self.data_dir) / "amazon_review" /domain / "negative.txt"
+
+                f = open(data_path,"r")
+                js = f.read()
+                dic = json.loads(js)
+
+                f.close()
+
+                for i in range(25):
+                    data_dict["input_str"].append(dic["input_str"][i])
+                    data_dict["output_label"].append(dic["output_label"][i])
+                    data_dict["domain_label"].append(dic["domain_label"][i])
+                    data_dict["domain_label_id"].append(domain_idx)
+            for domain_idx, domain in enumerate(domains):
+                data_path = Path(self.data_dir) / "amazon_review" /domain / "positive.txt"
+
+                f = open(data_path, "r")
+                js = f.read()
+                dic = json.loads(js)
+
+                f.close()
+                for i in range(25):
+                    data_dict["input_str"].append(dic["input_str"][i])
+                    data_dict["output_label"].append(dic["output_label"][i])
+                    data_dict["domain_label"].append(dic["domain_label"][i])
+                    data_dict["domain_label_id"].append(domain_idx)
+
+
+        elif mode == 'train':
+
+            for domain_idx, domain in enumerate(domains):
+                data_path = Path(self.data_dir) / "amazon_review" / domain / "UN.txt"
+
+                data_tree = ET.parse(data_path)
+                data_root = data_tree.getroot()
+                for rev in data_root.iter('review'):
+                    data_dict["input_str"].append(rev.text)
+                    data_dict["domain_label"].append(domain)
+                    data_dict["domain_label_id"].append(domain_idx)
+
+        elif mode == "test":
+            for domain_idx, domain in enumerate(domains):
+                data_path = Path(self.data_dir) / "amazon_review" / domain / "UN.txt"
+
+                data_tree = ET.parse(data_path)
+                data_root = data_tree.getroot()
+                for rev in data_root.iter('review'):
+                    data_dict["input_str"].append(rev.text)
+                    data_dict["domain_label"].append(domain)
+                    data_dict["domain_label_id"].append(domain_idx)
+
+
+        return data_dict
+
+
+    def load_data(self) -> Dict[str, Dict[str, List[Union[str, List[str], List[int]]]]]:
+        return {split: self.read_data_from_file(mode=split) for split in ReviewunlabelledDataProcessor.ALL_SPLITS}
+
+
+    def get_split_data(self, split: str) -> Dict[str, List[Union[str, List[str], List[int]]]]:
+        assert split in ReviewunlabelledDataProcessor.ALL_SPLITS
+        return self.data[split]
+
+
+# def get_split_domain_data(self, split: str, domain: str) -> Dict[str, List[Union[str, List[str], List[int]]]]:
+#     assert split in RumorDataProcessor.ALL_SPLITS
+#     assert domain in self.src_domains + [self.trg_domain]
+#     l, r = 0, len(self.data[split]["domain_label"]) - 1
+#     while self.data[split]["domain_label"][l] != domain:
+#         l += 1
+#     while self.data[split]["domain_label"][r] != domain:
+#         r -= 1
+#     split_domain_data = defaultdict(list)
+#     for k, v in self.data[split].items():
+#         split_domain_data[k] = v[l:r+1]
+#     return split_domain_data
+
+
+class ReviewunlabelledDataset(Dataset):
+    def __init__(self, split: str, data_processor: ReviewunlabelledDataProcessor, tokenizer: T5TokenizerFast, max_seq_len: int):
+        self.split = split
+        self.data_processor = data_processor
+        self.tokenizer = tokenizer
+        self.max_seq_len = max_seq_len
+        self.tokenized_data = self._init_tokenized_data(split, data_processor, tokenizer, max_seq_len)
+
+    def __len__(self):
+        return len(self.tokenized_data["domain_label_id"])
+
+    def __getitem__(self, index):
+        return {
+            k: v[index]
+            for k, v in self.tokenized_data.items()
+        }
+
+    @staticmethod
+    def _init_tokenized_data(split, data_processor, tokenizer, max_seq_len):
+        data = data_processor.get_split_data(split)
+        tokenized_data = tokenizer(data["input_str"],
+                                   padding="max_length", truncation=True,
+                                   max_length=max_seq_len, return_tensors="pt", return_attention_mask=True)
+        tokenized_data["input_str"] = data["input_str"]
+        tokenized_data["domain_label"] = data["domain_label"]
+
+        tokenized_data["domain_label_id"] = data["domain_label_id"]
+        return tokenized_data
